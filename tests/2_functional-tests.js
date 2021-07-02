b90364b6f308535e0bee2170e3d5b9377c7e161b
Mehul Shekhada <mehulshekhada96@gmail.com>
2021-07-02 18:28:42 +0530

    2 to face

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,28 +70,26 @@ const browser = new Browser();
 Browser.localhost("example.com", process.env.PORT || 3000);
 suiteSetup(function (done) {
   return browser.visit("/traveller", done);
+  
+});
+suite("Functional Tests with Zombie.js", function () {
+  suite('"Famous Italian Explorers" form', function () {
+    // #5
+    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+      browser.fill("surname", "Colombo").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text("span#name", "Cristoforo");
+        browser.assert.text("span#surname", "Colombo");
+        browser.assert.elements("span#dates", 1);
 
-  suite("Functional Tests with Zombie.js", function () {
-    suite('"Famous Italian Explorers" form', function () {
-      // #5
-      test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-        browser.fill("surname", "Colombo").pressButton("submit", function () {
-          browser.assert.success();
-          browser.assert.text("span#name", "Cristoforo");
-          browser.assert.text("span#surname", "Colombo");
-          browser.assert.elements("span#dates", 1);
-  
-          done();
-        });
-      });
-      // #6
-      test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-        assert.fail();
-  
         done();
       });
     });
-  });
-  
+    // #6
+    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+      assert.fail();
 
+      done();
+    });
+  });
 });
